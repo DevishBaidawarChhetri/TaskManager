e10276ee407fa7605c6e3630c9acfa58509eb14e
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2020-01-02 17:25:19 +0545

    Login and Signup Activity added

--- app/src/main/java/np/com/devish/taskmanager/LoginActivity.java
+++ app/src/main/java/np/com/devish/taskmanager/LoginActivity.java
@@ -3,12 +3,49 @@ package np.com.devish.taskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etLoginUsername, etLoginPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etLoginUsername = findViewById(R.id.etLoginUsername);
+        etLoginPassword = findViewById(R.id.etLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validation()){
+
+                }else{
+                    Toast.makeText(LoginActivity.this, "Username or password incorrect.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private boolean validation(){
+        if (TextUtils.isEmpty(etLoginUsername.getText())){
+            etLoginUsername.setError("Enter username");
+            etLoginUsername.requestFocus();
+            return false;
+        }else if(TextUtils.isEmpty(etLoginPassword.getText())){
+            etLoginPassword.setError("Enter password");
+            etLoginPassword.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/np/com/devish/taskmanager/SignupActivity.java
+++ app/src/main/java/np/com/devish/taskmanager/SignupActivity.java
@@ -6,13 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
     ImageView imageProfile;
+    EditText etFirstName, etLastName, etUsername, etPassword, etConfirmPassword;
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,28 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         imageProfile = findViewById(R.id.imgProfile);
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etCOnfirmPassword);
+        btnSignup = findViewById(R.id.btnSignup);
+
         imageProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 BrowseImage();
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validation()){
+                    Intent intent = new Intent (SignupActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(SignupActivity.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -44,5 +67,31 @@ public class SignupActivity extends AppCompatActivity {
         Uri uri = data.getData();
         imageProfile.setImageURI(uri);
 //        imagePath = getRealPathFromUrl(uri);
+    }
+
+
+    private boolean validation(){
+        if(TextUtils.isEmpty(etFirstName.getText())){
+            etFirstName.setError("Enter first name.");
+            etFirstName.requestFocus();
+            return false;
+        }else if(TextUtils.isEmpty(etLastName.getText())){
+            etLastName.setError("Enter last name.");
+            etLastName.requestFocus();
+            return false;
+        }else if(TextUtils.isEmpty(etUsername.getText())){
+            etUsername.setError("Enter username.");
+            etUsername.requestFocus();
+            return false;
+        }else if(TextUtils.isEmpty(etPassword.getText())) {
+            etPassword.setError("Enter password");
+            etPassword.requestFocus();
+            return false;
+        }else if(TextUtils.isEmpty(etConfirmPassword.getText())){
+            etConfirmPassword.setError("Enter confirm password");
+            etConfirmPassword.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
